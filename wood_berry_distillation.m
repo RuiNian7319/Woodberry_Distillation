5110bc54cca7134955db60d8976ed9fb51301cdc
Rui Nian <rnian@ualberta.ca>
2019-01-21 11:19:12 -0700

    updates to matlab code

--- wood_berry_distillation.m
+++ wood_berry_distillation.m
@@ -18,31 +18,45 @@ G = [G11, G12; G21, G22];
 sys2 = ss(A, B, C(1, :), D(1, :), 'InputDelay', [1, 3]);
 sys3 = ss(A, B, C(2, :), D(2, :), 'InputDelay', [7, 3]);
 
-%%%%%%%% Simulation of y1 %%%%%%%%
-t = 0:1:150;
-u1 = ones(2, numel(t));
-x0 = [0, 0, 0, 0];
-[y1, x1] = lsim(sys2, u1, t, x0);
-plot(t, y1, t, u1)
-legend('Response', 'Input')
-
-%%%%%%%% Simulation of y2 %%%%%%%%
-t = 0:1:150;
-u2 = ones(2, numel(t));
-x0 = [0, 0, 0, 0];
-[y2, x2] = lsim(sys3, u2, t, x0);
-figure();
-plot(t, y2, t, u2)
-legend('Response', 'Input')
-
-%%%%%%%% Simulation of Transfer Function %%%%%%%%
+%%%%%%%% Simulation Parameters %%%%%%%%
 t = 0:1:150;
 u = ones(2, numel(t));
+% u = zeros(2, numel(t));
+% u(1, :) = 0.157;
+% u(2, :) = 0.05337;
 x0 = [0, 0, 0, 0];
+
+
+%%%%%%%% Simulation of y1 %%%%%%%%
+[y1, x1] = lsim(sys2, u, t, x0);
+% figure()
+% plot(t, y1, t, u1)
+% legend('Response', 'Input')
+
+%%%%%%%% Simulation of y2 %%%%%%%%
+[y2, x2] = lsim(sys3, u, t, x0);
+% figure();
+% plot(t, y2, t, u2)
+% legend('Response', 'Input')
+
+%%%%%%%% Simulation of Transfer Function %%%%%%%%
 [y, x] = lsim(sys1, u, t, x0);
-figure();
-plot(t, y, t, u)
-legend('Response', 'Input')
+% figure();
+% plot(t, y, t, u)
+% legend('Response', 'Input')
+
+%%%%%%%%  Figure Generation  %%%%%%%%
+figure()
+plot(t, y(:, 1), t, y1, '--r')
+legend('Transfer Function', 'State Space', 'Interpreter', 'latex');
+xlabel('Time, \textit{t} (s)', 'Interpreter', 'latex');
+ylabel('%MeOH in Distillate, X_D (%)', 'FontName', 'Times New Roman');
+
+figure()
+plot(t, y(:, 2), t, y2, '--r')
+legend('Transfer Function', 'State Space', 'Interpreter', 'latex');
+xlabel('Time, \textit{t} (s)', 'Interpreter', 'latex');
+ylabel('%MeOH in Bottoms, X_B (%)', 'FontName', 'Times New Roman');
 
 %%%%%%%% Simulation of the SISO System %%%%%%%%
 % num = [0 1];
@@ -50,8 +64,8 @@ legend('Response', 'Input')
 % sys4 = tf(num, den, 'IODelay', 0.8);
 
 %%%%%%%% Step test of the systems %%%%%%%%
-step(sys1);
-figure()
-step(sys2);
-figure()
-step(sys3);
+% step(sys1);
+% figure()
+% step(sys2);
+% figure()
+% step(sys3);
