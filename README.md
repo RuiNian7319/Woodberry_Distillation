c41288b154436c57ab8064be700d94dd335169ce
Rui Nian <rnian@ualberta.ca>
2019-01-21 09:15:38 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wood-Berry Distillation Tower Simulation Package
+
+Contains:
+
+1. MATLAB Transfer Functions of Wood-Berry Distillation Tower
+2. MATLAB State Space representation 
+3. Python State Space representation
---
+++ wood_berry_distillation.m
@@ -0,0 +1,57 @@
+%%%%%%%% Original Transfer Function Realization %%%%%%%%
+
+num = {12.8, -18.9; 6.6, -19.4};
+den = {[16.7 1], [21, 1]; [10.9, 1], [14.4, 1]};
+
+sys1 = tf(num, den, 'IODelay', [1 3; 7 3]);
+
+%%%%%%%% Converting Transfer Functions to State Space %%%%%%%%
+
+G11 = tf([0 12.8], [16.7 1]);
+G12 = tf([0 -18.9], [21 1]);
+G21 = tf([0 6.6], [10.9 1]);
+G22 = tf([0 -19.4], [14.4 1]);
+
+G = [G11, G12; G21, G22];
+[A, B, C, D] = ssdata(ss(G, 'min'));
+
+sys2 = ss(A, B, C(1, :), D(1, :), 'InputDelay', [1, 3]);
+sys3 = ss(A, B, C(2, :), D(2, :), 'InputDelay', [7, 3]);
+
+%%%%%%%% Simulation of y1 %%%%%%%%
+t = 0:1:150;
+u1 = ones(2, numel(t));
+x0 = [0, 0, 0, 0];
+[y1, x1] = lsim(sys2, u1, t, x0);
+plot(t, y1, t, u1)
+legend('Response', 'Input')
+
+%%%%%%%% Simulation of y2 %%%%%%%%
+t = 0:1:150;
+u2 = ones(2, numel(t));
+x0 = [0, 0, 0, 0];
+[y2, x2] = lsim(sys3, u2, t, x0);
+figure();
+plot(t, y2, t, u2)
+legend('Response', 'Input')
+
+%%%%%%%% Simulation of Transfer Function %%%%%%%%
+t = 0:1:150;
+u = ones(2, numel(t));
+x0 = [0, 0, 0, 0];
+[y, x] = lsim(sys1, u, t, x0);
+figure();
+plot(t, y, t, u)
+legend('Response', 'Input')
+
+%%%%%%%% Simulation of the SISO System %%%%%%%%
+% num = [0 1];
+% den = [10 1];
+% sys4 = tf(num, den, 'IODelay', 0.8);
+
+%%%%%%%% Step test of the systems %%%%%%%%
+step(sys1);
+figure()
+step(sys2);
+figure()
+step(sys3);
