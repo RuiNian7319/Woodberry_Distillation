2d3bb3c831dce518f7a15746ab3627bc17e1e9f2
RUi Nian <rnian@ualberta.ca>
2019-01-21 14:53:04 -0700

    updates

--- Woodberry_Distillation.py
+++ Woodberry_Distillation.py
@@ -6,5 +6,37 @@ By: Rui Nian
 Date of Last Edit: January 21st 2019
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+from copy import deepcopy
+from scipy.integrate import odeint
+
+import gc
+
+import warnings
+
+
+class WoodBerryDistillation:
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __init__(self, nsim, x0, u0, xs=np.array([1, 0]), us=np.array([0.157, 0.053]), step_size=1):
+        """
+
+
+        """
+        pass
+
+
+    def ode(self):
+        pass
+
+
 
 
