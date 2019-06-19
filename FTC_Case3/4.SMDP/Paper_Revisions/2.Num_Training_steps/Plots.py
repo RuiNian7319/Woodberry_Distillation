b48cf6162b1191302e812ef56b86b05624148bed
Rui Nian <rnian@ualberta.ca>
2019-06-19 15:55:14 -0600

    updates

--- FTC_Case3/4.SMDP/Paper_Revisions/2.Num_Training_steps/Plots.py
+++ FTC_Case3/4.SMDP/Paper_Revisions/2.Num_Training_steps/Plots.py
@@ -15,11 +15,7 @@ fonts = {"family": "serif",
 plt.rc('font', **fonts)
 plt.rc('text', usetex=True)
 
-A = pd.read_csv('time_data/0.5.csv', names=['Time'])
-B = pd.read_csv('time_data/1.csv', names=['Time'])
-C = pd.read_csv('time_data/2.csv', names=['Time'])
-D = pd.read_csv('time_data/4.csv', names=['Time'])
-E = pd.read_csv('time_data/8.csv', names=['Time'])
+data = pd.read_csv('time.csv')
 
 """
 Normal plots
@@ -73,51 +69,6 @@ num[3 * 1000:1000 * 4] = 4
 num[4 * 1000:1000 * 5] = 8
 
 data['Fault Magnitude, F (lb/min)'] = num
-
-"""
-Normalized Plots
-"""
-
-A_mean_norm = np.mean(A) / 1
-B_mean_norm = np.mean(B) / 1
-C_mean_norm = np.mean(C) / 2
-D_mean_norm = np.mean(D) / 3
-E_mean_norm = np.mean(E) / 5
-
-A_std_norm = np.std(A) / 1
-B_std_norm = np.std(B) / 1
-C_std_norm = np.std(C) / 2
-D_std_norm = np.std(D) / 3
-E_std_norm = np.std(E) / 5
-
-A_norm = np.zeros(1000)
-B_norm = np.zeros(1000)
-C_norm = np.zeros(1000)
-D_norm = np.zeros(1000)
-E_norm = np.zeros(1000)
-
-aList = [A_norm, B_norm, C_norm, D_norm, E_norm]
-
-for i in range(len(A_norm)):
-    A_norm[i] = np.random.normal(A_mean_norm, A_std_norm, 1)
-
-for i in range(len(B_norm)):
-    B_norm[i] = np.random.normal(B_mean_norm, B_std_norm, 1)
-
-for i in range(len(C_norm)):
-    C_norm[i] = np.random.normal(C_mean_norm, C_std_norm, 1)
-
-for i in range(len(D_norm)):
-    D_norm[i] = np.random.normal(D_mean_norm, D_std_norm, 1)
-
-for i in range(len(E_norm)):
-    E_norm[i] = np.random.normal(E_mean_norm, E_std_norm, 1)
-
-data_norm = np.concatenate([A_norm, B_norm, C_norm, D_norm, E_norm]).reshape(-1, 1)
-
-data_norm = pd.DataFrame(data_norm, columns=['Time, t (min)'])
-
-data_norm['Fault Magnitude, F (lb/min)'] = num
 
 """
 Plotting
